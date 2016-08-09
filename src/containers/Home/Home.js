1d703f0bda53f52d8624cb5a52f4e3680b2bb89a
codejunkienick <psychedelicespresso@gmail.com>
2016-08-09 10:29:56 +0500

    rename home page to home page

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -87,7 +87,7 @@ export default class Home extends Component {
 
     return (
       <div className={styles.home}>
-        <Helmet title="Store"/>
+        <Helmet title="Home"/>
         <header className={styles.header}>
           <FlatButton
             label="Корзина"
